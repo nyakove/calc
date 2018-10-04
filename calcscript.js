edd60ba9169eb2246560e35be316f7606d706cdb
admin3 <admin3@unity.local>
2018-10-04 17:36:28 +0300

    clear and del functions

--- calcscript.js
+++ calcscript.js
@@ -16,6 +16,10 @@ function clear() {
 	document.getElementsByTagName('input')[0].style.backgroundColor = '#B0E0E6';
 }
 
+function del() {
+	if (document.getElementsByTagName('input')[0].value.length == 0) return false;
+	document.getElementsByTagName('input')[0].value = document.getElementsByTagName('input')[0].value.substring(0, document.getElementsByTagName('input')[0].value.length - 1)
+}
 //var buttons = document.getElementsByClassName('digit');
 
 document.getElementById('1').addEventListener('click', clickInput);
@@ -33,3 +37,5 @@ document.getElementById('minus').addEventListener('click', clickInput);
 document.getElementById('divide').addEventListener('click', clickInput);
 document.getElementById('multiple').addEventListener('click', clickInput);
 document.getElementById('result').addEventListener('click', calculate);
+document.getElementById('clear').addEventListener('click', clear);
+document.getElementById('del').addEventListener('click', del);
