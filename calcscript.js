0f4e8e43afa7e838cda2b39b442a95001b993506
admin3 <admin3@unity.local>
2018-10-04 16:24:25 +0300

    Initial commit

---
+++ calcscript.js
@@ -0,0 +1,35 @@
+function clickInput() {
+	document.getElementsByTagName('input')[0].value += this.innerHTML;
+}
+
+function calculate() {
+	value = eval(document.getElementsByTagName('input')[0].value);
+	if (isNaN(value) || value == Infinity) {
+		value = 'Error!';
+		document.getElementsByTagName('input')[0].style.backgroundColor = '#F08080';
+	}
+	document.getElementsByTagName('input')[0].value = value;
+}
+
+function clear() {
+	document.getElementsByTagName('input')[0].value = '';
+	document.getElementsByTagName('input')[0].style.backgroundColor = '#B0E0E6';
+}
+
+//var buttons = document.getElementsByClassName('digit');
+
+document.getElementById('1').addEventListener('click', clickInput);
+document.getElementById('2').addEventListener('click', clickInput);
+document.getElementById('3').addEventListener('click', clickInput);
+document.getElementById('4').addEventListener('click', clickInput);
+document.getElementById('5').addEventListener('click', clickInput);
+document.getElementById('6').addEventListener('click', clickInput);
+document.getElementById('7').addEventListener('click', clickInput);
+document.getElementById('8').addEventListener('click', clickInput);
+document.getElementById('9').addEventListener('click', clickInput);
+document.getElementById('0').addEventListener('click', clickInput);
+document.getElementById('plus').addEventListener('click', clickInput);
+document.getElementById('minus').addEventListener('click', clickInput);
+document.getElementById('divide').addEventListener('click', clickInput);
+document.getElementById('multiple').addEventListener('click', clickInput);
+document.getElementById('result').addEventListener('click', calculate);
