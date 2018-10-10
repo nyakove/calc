0cdf664545616fe4c198113e62eb79d54cb6edd4
admin3 <admin3@unity.local>
2018-10-10 11:46:10 +0300

    one listener instead of many

--- calcscript.js
+++ calcscript.js
@@ -1,8 +1,8 @@
 function clickInput() {
-	if ('+-/*'.indexOf(this.innerHTML) > -1 && '+-/*'.indexOf(document.getElementsByTagName('input')[0].value.slice(-1)) > -1) {
+	if ('+-/*'.indexOf(event.target.innerHTML) > -1 && '+-/*'.indexOf(document.getElementsByTagName('input')[0].value.slice(-1)) > -1) {
 		del();
 	}
-	if (document.getElementsByTagName('input')[0].value == '' && '+-/*'.indexOf(this.innerHTML) > -1) {
+	if (document.getElementsByTagName('input')[0].value == '' && '+-/*.'.indexOf(event.target.innerHTML) > -1) {
 		return;
 	}
 	if (document.getElementsByTagName('input')[0].value.length == 1 && document.getElementsByTagName('input')[0].value.slice(-1) == '0' && '123456789'.indexOf(this.innerHTML) > -1) {
@@ -11,7 +11,10 @@ function clickInput() {
 	if (document.getElementsByTagName('input')[0].value == 'Error!') {
 		clear();
 	}
-	document.getElementsByTagName('input')[0].value += this.innerHTML;
+	if (event.target.innerHTML == 'C' || event.target.innerHTML == 'CL' || event.target.innerHTML == '=') {
+		return;
+	}
+	document.getElementsByTagName('input')[0].value += event.target.innerHTML;
 }
 
 function calculate() {
@@ -98,51 +101,19 @@ function keyOff(event) {
  }
 
 function clickOn() {
-	this.style.backgroundColor = '#EEE8AA';
+	event.target.style.backgroundColor = '#EEE8AA';
 }
 
 function clickOff() {
-	this.style.backgroundColor = '#FFFFE0';
+	event.target.style.backgroundColor = '#FFFFE0';
 }
 
-document.getElementById('1').addEventListener('mousedown', clickOn);
-document.getElementById('1').addEventListener('mouseup', clickOff);
-document.getElementById('2').addEventListener('mousedown', clickOn);
-document.getElementById('2').addEventListener('mouseup', clickOff);
-document.getElementById('3').addEventListener('mousedown', clickOn);
-document.getElementById('3').addEventListener('mouseup', clickOff);
-document.getElementById('4').addEventListener('mousedown', clickOn);
-document.getElementById('4').addEventListener('mouseup', clickOff);
-document.getElementById('5').addEventListener('mousedown', clickOn);
-document.getElementById('5').addEventListener('mouseup', clickOff);
-document.getElementById('6').addEventListener('mousedown', clickOn);
-document.getElementById('6').addEventListener('mouseup', clickOff);
-document.getElementById('7').addEventListener('mousedown', clickOn);
-document.getElementById('7').addEventListener('mouseup', clickOff);
-document.getElementById('8').addEventListener('mousedown', clickOn);
-document.getElementById('8').addEventListener('mouseup', clickOff);
-document.getElementById('9').addEventListener('mousedown', clickOn);
-document.getElementById('9').addEventListener('mouseup', clickOff);
-document.getElementById('0').addEventListener('mousedown', clickOn);
-document.getElementById('0').addEventListener('mouseup', clickOff);
+document.getElementById('keys').addEventListener('click', clickInput);
+document.getElementById('keys').addEventListener('mousedown', clickOn);
+document.getElementById('keys').addEventListener('mouseup', clickOff);
 document.addEventListener('keydown', keyOn);
 document.addEventListener('keyup', keyOff);
 document.addEventListener('keydown', keyOps);
-document.getElementById('1').addEventListener('click', clickInput);
-document.getElementById('2').addEventListener('click', clickInput);
-document.getElementById('3').addEventListener('click', clickInput);
-document.getElementById('4').addEventListener('click', clickInput);
-document.getElementById('5').addEventListener('click', clickInput);
-document.getElementById('6').addEventListener('click', clickInput);
-document.getElementById('7').addEventListener('click', clickInput);
-document.getElementById('8').addEventListener('click', clickInput);
-document.getElementById('9').addEventListener('click', clickInput);
-document.getElementById('0').addEventListener('click', clickInput);
-document.getElementById('dot').addEventListener('click', clickInput);
-document.getElementById('plus').addEventListener('click', clickInput);
-document.getElementById('minus').addEventListener('click', clickInput);
-document.getElementById('divide').addEventListener('click', clickInput);
-document.getElementById('multiple').addEventListener('click', clickInput);
 document.getElementById('result').addEventListener('click', calculate);
 document.getElementById('clear').addEventListener('click', clear);
 document.getElementById('del').addEventListener('click', del);
